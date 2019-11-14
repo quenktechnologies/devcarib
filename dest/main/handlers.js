04110a5599f63395895969b9002f6ae1ece82751
Lasana Murray <lmurray@quenk.com>
2019-11-14 15:42:37 -0400

    chore: rebuild

--- dest/main/handlers.js
+++ dest/main/handlers.js
@@ -5,7 +5,7 @@ const response_1 = require("@quenk/tendril/lib/app/api/action/response");
 const control_1 = require("@quenk/tendril/lib/app/api/action/control");
 const pool_1 = require("@quenk/tendril/lib/app/api/action/pool");
 const monad_1 = require("@quenk/noni/lib/control/monad");
-const type_1 = require("@quenk/noni/lib/data/type");
+const employer_1 = require("./validation/employer");
 /**
  * showForm displays the employer regisration form.
  */
@@ -14,14 +14,16 @@ exports.showForm = (_) => response_1.show('employer/registration/form.html', {})
  *   createEmployer creates the employer after registration.
 */
 exports.createEmployer = (r) => monad_1.doN(function* () {
-    if (type_1.isObject(r.body)) {
-        let data = r.body;
+    let eResult = employer_1.validate(r.body);
+    if (eResult.isRight()) {
+        let data = eResult.takeRight();
         let db = yield pool_1.checkout('main');
         let collection = db.collection('employers');
         yield control_1.await(() => collection_1.insertOne(collection, data));
         return response_1.redirect('/dashboard', 302);
     }
     else {
+        console.log(eResult.takeLeft().explain());
         return response_1.redirect('/', 302);
     }
 });
