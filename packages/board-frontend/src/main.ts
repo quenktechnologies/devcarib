48f2e5a292614768afb4f2402069fd88207b8b49
OmariLawrence <originalolawrence@gmail.com>
2019-11-22 15:04:03 -0400

    refactor: created action bar for dashboard

--- packages/board-frontend/src/main.ts
+++ packages/board-frontend/src/main.ts
@@ -1,1 +1,34 @@
-alert('dashboard started');
+import {View} from '@quenk/wml';
+import {BoardDashBoardView} from './views/board';
+
+export class BoardDashboard{
+
+    constructor(public content:HTMLElement){}
+
+    view: View = new BoardDashBoardView(this);
+
+    static create(id:string){
+
+        let e:HTMLElement = <HTMLElement>document.getElementById(id);
+
+        return new BoardDashboard(e);
+
+    }
+
+    setContent(view:View){
+
+        while(this.content.firstChild != null)
+            this.content.removeChild(this.content.firstChild);
+
+        this.content.appendChild(view.render());
+
+    }
+
+    run(){
+
+        this.setContent(this.view.render());
+
+    }
+}
+
+BoardDashboard.create('app').run();
