a61d0fc3675eece86732e51ce8ed3f6e91634c65
Lasana Murray <lmurray@quenk.com>
2020-05-31 22:23:21 -0400

    refactor: first pass at new landing page

--- packages/local/board-views/build.mk
+++ packages/local/board-views/build.mk
@@ -5,26 +5,30 @@
 ### Settings ###
 BOARD_VIEWS_JS_VARS:=$(HERE)/node_modules/@quenk/wml-widgets/lib/classNames.js
 BOARD_VIEWS_SRC_DIR:=$(BOARD_VIEWS_DIR)/views
-BOARD_VIEWS_LESS_DIR:=$(BOARD_VIEWS_DIR)/less
 BOARD_VIEWS_LESS_FILES:=$(shell $(FIND) $(BOARD_VIEWS_SRC_DIR) -name \*.less)
 BOARD_VIEWS_LESS_MAIN:=$(BOARD_VIEWS_DIR)/main.less
-BOARD_VIEWS_LESS_INDEX:=$(BOARD_VIEWS_DIR)/auto.less
+BOARD_VIEWS_LESS_IMPORTS:=$(BOARD_VIEWS_DIR)/auto.less
 BOARD_VIEWS_PUBLIC_DIR:=$(BOARD_VIEWS_DIR)/public
+BOARD_VIEWS_CSS_FILE:=$(BOARD_VIEWS_PUBLIC_DIR)/assets/css/site.css
 
 ### Graph ###
 $(BOARD_VIEWS_DIR): $(BOARD_VIEWS_PUBLIC_DIR)
 	touch $@
 
-$(BOARD_VIEWS_PUBLIC_DIR): $(BOARD_VIEWS_LESS_DIR) $(BOARD_VIEWS_LESS_MAIN)
-	rm -R $@ || true
-	mkdir -p $@/assets/css
-	$(LESSC) --source-map-less-inline \
-	--js-vars=$(BOARD_VIEWS_JS_VARS) $(BOARD_VIEWS_LESS_MAIN) \
-	| ./node_modules/.bin/cleancss > $@/assets/css/site.css
+$(BOARD_VIEWS_PUBLIC_DIR): $(BOARD_VIEWS_CSS_FILE)
 	touch $@
 
-$(BOARD_VIEWS_LESS_DIR): $(BOARD_VIEWS_LESS_FILES)
-	echo "" > $(BOARD_VIEWS_LESS_INDEX)
-	$(foreach f,$(subst $(BOARD_VIEWS_LESS_DIR),,$^),\
-	echo '@import "./less$(f)";' >> $(BOARD_VIEWS_LESS_INDEX) && ) true
-	touch $@
+$(BOARD_VIEWS_CSS_FILE): $(BOARD_VIEWS_LESS_IMPORTS) $(BOARD_VIEWS_LESS_MAIN)
+	mkdir -p $(dir $@)
+	rm -R $@ || true
+	$(LESSC) --source-map-less-inline \
+	--js-vars=$(BOARD_VIEWS_JS_VARS) $(BOARD_VIEWS_LESS_MAIN) \
+	| ./node_modules/.bin/cleancss > $@
+
+$(BOARD_VIEWS_LESS_IMPORTS): $(BOARD_VIEWS_LESS_FILES)
+	echo "" > $@
+	$(foreach f,$(subst $(BOARD_VIEWS_DIR),,$^),\
+	echo '@import "./$(f)";' >> $@ && ) true
+
+
+
