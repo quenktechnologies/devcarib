73864eb651ceaccfce8e61b426acb2ce2163d654
Genora <genora@Owners-MacBook-Pro-2.local>
2020-11-22 22:59:55 -0400

    chore: update test for post form

--- packages/apps/board-form-post/test/frontend/build/post-from_test.ts
+++ packages/apps/board-form-post/test/frontend/build/post-from_test.ts
@@ -15,6 +15,25 @@ describe('post-form', () => {
         let company = <HTMLInputElement>form.querySelector('[name="company"]');
         company.value = 'Quenk';
 
+        let email = <HTMLInputElement>form.querySelector('[name="company_email"]');
+        email.value = 'info@example.com';
+
+        let logo= <HTMLInputElement>form.querySelector('[name="company_logo"]');
+        logo.value = 'https://www.example.com';
+
+        let applyLink = <HTMLInputElement>form.querySelector('[name="apply_url"]');
+        applyLink.value = 'https://www.quenk.com/';
+
+        let jobTitle = <HTMLInputElement>form.querySelector('[name="title"]');
+        jobTitle.value = 'Software Engineer needed';
+
+        let jobDescription = <HTMLInputElement>form.querySelector('[name="description"]');
+        email.value = 'We are currently looking for a Software Engineer in the Port of Spain region';
+
+        let btn = document.querySelector('.-preview-button');
+        assert_1.assert(btn).not.undefined();
+        btn.click();
+
     })
 
 })
--- packages/apps/board-form-post/test/frontend/bundle/post-from_test.js
+++ packages/apps/board-form-post/test/frontend/bundle/post-from_test.js
@@ -1539,7 +1539,22 @@ describe('post-form', function () {
         assert_1.assert(form).not.undefined();
         var company = form.querySelector('[name="company"]');
         company.value = 'Quenk';
+        var email = form.querySelector('[name="company_email"]');
+        email.value = 'info@example.com';
+        var logo = form.querySelector('[name="company_logo"]');
+        logo.value = 'https://www.quenk.com/';
+        var applyLink = form.querySelector('[name="apply_url"]');
+        applyLink.value = 'https://www.quenk.com';
+        var jobTitle = form.querySelector('[name="title"]');
+        jobTitle.value = 'Software Engineer needed';
+        var jobDescription = form.querySelector('[name="description"]');
+        jobDescription.value = 'We are currently looking for a Software Engineer in the Port of Spain region';
+        let btn = document.querySelector('.-preview-button');
+        assert_1.assert(btn).not.undefined();
+        btn.click();
+
     });
 });
+
 
 },{"@quenk/test/lib/assert":1}]},{},[28]);
