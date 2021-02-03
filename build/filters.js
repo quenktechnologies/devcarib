74b0fa9ea078f26d28279dcaf79821cf4712d3a0
Lasana Murray <lmurray@quenk.com>
2021-02-02 23:44:29 -0400

    chore: rebuild

--- build/filters.js
+++ build/filters.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.timestamp = void 0;
+exports.timefromnow = exports.timestamp = void 0;
 const moment = require("moment");
 /**
  * timestamp displays a date in the follwing format: "January 30th 2021".
@@ -10,5 +10,14 @@ exports.timestamp = (str) => {
     if ((str === '') || (!stamp.isValid()))
         return '';
     return stamp.format('MMMM Do YYYY');
+};
+/**
+ * timefromnow displays the distance of a date from the current date.
+ */
+exports.timefromnow = (str) => {
+    let date = moment(str);
+    if ((str === '') || (!date.isValid()))
+        return '';
+    return date.fromNow();
 };
 //# sourceMappingURL=filters.js.map
