1c2842915d60337b1cf5ac659fc8ca1987b2b7e5
Lasana Murray <lmurray@quenk.com>
2021-02-02 23:44:23 -0400

    refactor: improve styling and layout consistency a little

--- src/filters.ts
+++ src/filters.ts
@@ -12,3 +12,16 @@ export const timestamp = (str: string) => {
     return stamp.format('MMMM Do YYYY');
 
 }
+
+/**
+ * timefromnow displays the distance of a date from the current date.
+ */
+export const timefromnow = (str:string) => {
+
+    let date = moment(str);
+
+    if ((str === '') || (!date.isValid())) return '';
+
+    return date.fromNow();
+
+}
