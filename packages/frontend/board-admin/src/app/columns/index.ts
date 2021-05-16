66e69e06d8d8788421568e30534bb20876de72ae
Lasana Murray <lmurray@quenk.com>
2021-05-15 20:54:41 -0400

    chore: move columns to own submodule

---
+++ packages/frontend/board-admin/src/app/columns/index.ts
@@ -0,0 +1,80 @@
+import { Value } from '@quenk/noni/lib/data/jsonx';
+
+import { Column, CellContext } from '@quenk/wml-widgets/lib/data/table';
+
+import { Post } from '@board/types/lib/post';
+
+import { TitleColumnView, ActionSpec, ActionColumnView } from './views';
+
+export { ActionSpec }
+
+/**
+ * TitleColumnAction is a function invoked when the title of a post is clicked
+ * on.
+ */
+export type TitleColumnAction = (p: Post) => void;
+
+/**
+ * TitleColumn displays the title of the post.
+ */
+export class TitleColumn implements Column<Value, Post> {
+
+    constructor(public action: TitleColumnAction) { }
+
+    name = 'title';
+
+    heading = 'Title';
+
+    cellFragment = (c: CellContext<Value, Post>) => new TitleColumnView({
+
+        post: c.datum,
+
+        onClick: () => this.action(c.datum)
+
+    });
+
+}
+
+/**
+ * CompanyColumn displays the company name.
+ */
+export class CompanyColumn implements Column<Value, Post> {
+
+    name = 'company';
+
+    heading = 'Company';
+
+}
+
+/**
+ * ApprovedColumn displays the approval status of the post.
+ */
+export class ApprovedColumn implements Column<Value, Post> {
+
+    name = 'approved';
+
+    heading = 'Approved?';
+
+}
+
+/**
+ * ActionColumn displays a drop-down menu with actions that can be taken on a
+ * single post.
+ */
+export class ActionColumn implements Column<Value, Post> {
+
+    constructor(public actions: ActionSpec[]) { }
+
+    name = '';
+
+    heading = 'Actions';
+
+    cellFragment = (c: CellContext<Value, Post>) => new ActionColumnView({
+
+        actions: this.actions,
+
+        post: c.datum
+
+    });
+
+}
