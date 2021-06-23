aa85d9753a7111fbbdc6ee493b99c12d94a84a52
Lasana Murray <lmurray@quenk.com>
2021-06-22 22:34:04 -0400

    refactor: convert admin views to wml

--- packages/extras/board-views/build.mk
+++ packages/extras/board-views/build.mk
@@ -7,15 +7,30 @@ LESSC?=./node_modules/.bin/lessc
 
 ### Settings ###
 BOARD_VIEWS_JS_VARS:=$(HERE)/node_modules/@quenk/wml-widgets/lib/classNames.js
-BOARD_VIEWS_SRC_DIR:=$(BOARD_VIEWS_DIR)/views
-BOARD_VIEWS_LESS_FILES:=$(shell $(FIND) $(BOARD_VIEWS_SRC_DIR) -name \*.less)
+BOARD_VIEWS_VIEWS_DIR:=$(BOARD_VIEWS_DIR)/views
+BOARD_VIEWS_LIB_DIR:=$(BOARD_VIEWS_DIR)/lib
+BOARD_VIEWS_SRC_DIR:=$(BOARD_VIEWS_DIR)/src
+BOARD_VIEWS_SRC_DIR_FILES:=$(shell $(FIND) $(BOARD_VIEWS_SRC_DIR) -name \*.wml)
+BOARD_VIEWS_LESS_FILES:=$(shell $(FIND) $(BOARD_VIEWS_VIEWS_DIR) -name \*.less)
 BOARD_VIEWS_LESS_MAIN:=$(BOARD_VIEWS_DIR)/main.less
 BOARD_VIEWS_LESS_IMPORTS:=$(BOARD_VIEWS_DIR)/auto.less
 BOARD_VIEWS_PUBLIC_DIR:=$(BOARD_VIEWS_DIR)/public
 BOARD_VIEWS_CSS_FILE:=$(BOARD_VIEWS_PUBLIC_DIR)/assets/css/site.css
 
 ### Graph ###
-$(BOARD_VIEWS_DIR): $(BOARD_VIEWS_PUBLIC_DIR) $(BOARD_WIDGETS_DIR)
+$(BOARD_VIEWS_DIR): $(BOARD_VIEWS_LIB_DIR)\
+                    $(BOARD_VIEWS_PUBLIC_DIR)\
+		    $(BOARD_WIDGETS_DIR)
+	touch $@
+
+$(BOARD_VIEWS_LIB_DIR): $(BOARD_VIEWS_SRC_DIR)
+	rm -R $@ || true
+	cp -R -u $(BOARD_VIEWS_SRC_DIR) $@
+	$(WML) $@
+	$(TSC) --project $@
+	touch $@
+
+$(BOARD_VIEWS_SRC_DIR): $(BOARD_VIEWS_SRC_DIR_FILES)
 	touch $@
 
 $(BOARD_VIEWS_PUBLIC_DIR): $(BOARD_VIEWS_CSS_FILE)
