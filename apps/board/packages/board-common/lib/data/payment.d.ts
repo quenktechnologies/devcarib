a2cb7596f6b827115e95d055e46cb35f2abe1fc1
Lasana Murray <lmurray@quenk.com>
2022-03-20 21:12:55 -0400

    chore: [board] remove board-common

--- apps/board/packages/board-common/lib/data/payment.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * supportedPaymentFrequencies is a list of acceptable period specifiers that
- * indicate how frequent a payment would be made.
- */
-export declare const supportedPaymentFrequencies: string[];
