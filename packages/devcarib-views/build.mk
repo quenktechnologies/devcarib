06f4487cc926cee711a49ccb2a4312cd977c9cdf
Lasana Murray <lmurray@quenk.com>
2022-03-23 00:14:25 -0400

    chore: move mia-views to devcarib-views

---
+++ packages/devcarib-views/build.mk
@@ -0,0 +1,30 @@
+$(DEVCARIB_VIEWS_DIR): $(DEVCARIB_VIEWS_LIB_DIR)\
+                       $(DEVCARIB_VIEWS_PUBLIC_DIR)
+	touch $@
+
+$(DEVCARIB_VIEWS_LIB_DIR): $(DEVCARIB_VIEWS_SRC_DIR)
+	rm -R $@ || true
+	cp -R -u $(DEVCARIB_VIEWS_SRC_DIR) $@
+	$(WML) $@
+	$(TSC) --project $@
+	touch $@
+
+$(DEVCARIB_VIEWS_SRC_DIR): $(DEVCARIB_VIEWS_SRC_DIR_FILES)
+	touch $@
+
+$(DEVCARIB_VIEWS_PUBLIC_DIR): $(DEVCARIB_VIEWS_MIA_CSS_FILE) 
+	touch $@
+
+$(DEVCARIB_VIEWS_MIA_CSS_FILE): $(DEVCARIB_WIDGETS_DIR) \
+		                $(DEVCARIB_VIEWS_MIA_LESS_IMPORTS) \
+		                $(DEVCARIB_VIEWS_MIA_LESS_MAIN)
+	mkdir -p $(dir $@)
+	rm -R $@ || true
+	$(LESSC) --source-map-less-inline \
+	--js-vars=$(DEVCARIB_VIEWS_JS_VARS) $(DEVCARIB_VIEWS_MIA_LESS_MAIN) \
+	| ./node_modules/.bin/cleancss > $@
+
+$(DEVCARIB_VIEWS_MIA_LESS_IMPORTS): $(DEVCARIB_VIEWS_MIA_LESS_FILES)
+	echo "" > $@
+	$(foreach f,$(subst $(DEVCARIB_VIEWS_SRC_DIR),,$^),\
+	echo '@import "./$(f)";' >> $@ && ) true
