e6a74b11f7104da7958302f818ae3d952d0b641e
Lasana Murray <lmurray@quenk.com>
2022-10-30 11:16:35 -0400

    close #154: [converse] allow users to change their password

--- apps/converse/src/r/index.ts
+++ apps/converse/src/r/index.ts
@@ -11,11 +11,14 @@ import { PostModel } from '@converse/models/lib/post';
 import { CommentModel } from '@converse/models/lib/comment';
 import { EventModel } from '@converse/models/lib/event';
 import { InviteModel } from '@converse/models/lib/invite';
+import { UserModel } from '@converse/models/lib/user';
+
+import {User} from '@converse/types/lib/user';
 
 /**
  * UserController provides the API endpoint for the current user.
  */
-export class UserController {
+export class UserController extends ApiController<User> {
 
     get(req: Request): Action<void> {
 
@@ -27,13 +30,25 @@ export class UserController {
 
     }
 
+    update(req: Request) : Action<void> {
+
+        let muser = req.session.get('user');
+
+        if (muser.isNothing()) return notFound();
+
+        req.params.id =  String((<User>muser.get()).id);
+
+        return super.update(req);
+
+    }
+
 }
 
 export const postsCtrl = new ApiController(PostModel.getInstance);
 
 export const commentsCtrl = new ApiController(CommentModel.getInstance);
 
-export const userCtrl = new UserController();
+export const userCtrl = new UserController(UserModel.getInstance);
 
 export const eventCtrl = new ApiController(EventModel.getInstance);
 
