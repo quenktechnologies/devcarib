2ab4f586cd605860d8a10081f2edabb2f4be739b
Lasana Murray <lmurray@quenk.com>
2023-05-19 20:32:17 -0400

    #162: use ApiController from @quenk/backend [board]

--- apps/board/src/r/index.ts
+++ apps/board/src/r/index.ts
@@ -1,12 +1,30 @@
 /* tdc-output-imports */
 
+import * as mongodb from 'mongodb';
+
 // @ts-ignore: 2300
 import { Request } from '@quenk/tendril/lib/app/api/request';
 
-import { ApiController } from '@board/server/lib/controllers/api';
+import { ApiController } from '@quenk/backend/lib/app/controllers/api';
+import { MapModelProvider } from '@quenk/backend/lib/app/model';
+import { SkipAndLimit } from '@quenk/backend/lib/app/controllers/api/search/strategy';
 
-import { JobModel } from '@board/server/lib/data/models/job';
+import { modelsAvailable } from '@board/server/lib/data/models';
 
-export const jobsCtrl = new ApiController(JobModel.getInstance);
+/**
+ * BoardAPI
+ */
+export class BoardAPI extends ApiController<mongodb.Db> {
+
+    constructor() {
+        super(
+            'main',
+            new MapModelProvider(modelsAvailable),
+            new SkipAndLimit()
+        );
+    }
+}
+
+export const api = new BoardAPI();
 
 /* tdc-output-exports */
