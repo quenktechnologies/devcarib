6b2182c15cc3a026464f6a92159ee33606528f99
Lasana Murray <lmurray@quenk.com>
2023-05-21 14:39:44 -0400

    #162: use macro to generate types [board-types]

--- apps/board/packages/board-types/build.mk
+++ apps/board/packages/board-types/build.mk
@@ -1,17 +1,5 @@
 $(BOARD_TYPES_DIR): $(BOARD_TYPES_DIR)/lib
 	touch $@
 	
-$(BOARD_TYPES_DIR)/lib: $(BOARD_SCHEMA_DIR) \
-	                $(shell find $(BOARD_TYPES_DIR)/src -type f)
-	rm -R $@ || true
-	cp -R -u $(BOARD_TYPES_DIR)/src $@
-	mkdir -p $@
-	$(DAGEN) --templates node_modules/@quenk/dagen-templates-quenk/templates/data-types \
-	--template type.nunjucks \
-        --plugin ./node_modules/@quenk/dagen-commons/lib/plugins/imports \
-	--namespace types \
-	--ext ts \
-	--out $@ \
-	$(BOARD_SCHEMA_TARGETS)
-	$(TSC) --project $@
-	touch $@
+$(BOARD_TYPES_DIR)/lib: $(BOARD_SCHEMA_DIR)
+	$(call qtl_data_types,$@,$(BOARD_SCHEMA_MODEL_TARGETS))
