b7e87ed9743747130b52de620a7b2993bb470f42
Lasana Murray <lmurray@quenk.com>
2023-05-25 12:10:56 -0400

    chore: remove artifacts before building [board-types]

--- apps/board/packages/board-types/build.mk
+++ apps/board/packages/board-types/build.mk
@@ -1,5 +1,9 @@
+include ./node_modules/@quenk/dagen-templates-quenk/macros.mk
+
 $(BOARD_TYPES_DIR): $(BOARD_TYPES_DIR)/lib
 	touch $@
 	
 $(BOARD_TYPES_DIR)/lib: $(BOARD_SCHEMA_DIR)
+	rm -R $@ || true
+	mkdir -p $@
 	$(call qtl_data_types,$@,$(BOARD_SCHEMA_MODEL_TARGETS))
