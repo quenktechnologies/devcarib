3704223665f561b162c9fe45d6bd843e5076f9ac
Lasana Murray <lmurray@quenk.com>
2023-05-25 12:08:53 -0400

    #162: check in new mia-types [mia]

--- apps/mia/packages/mia-types/build.mk
+++ apps/mia/packages/mia-types/build.mk
@@ -1,39 +1,9 @@
-### Builds mia-types ###
+include ./node_modules/@quenk/dagen-templates-quenk/macros.mk
 
-# Make type files for schema in a dir.
-# $1 List of types to process
-# $2 The path to the dir the files are in.
-define mia_mktypes
-$(foreach d,$1,\
-  $(eval name=$(notdir $(basename $(d)))) \
-  $(DAGEN) --templates $(MIA_TYPES_TEMPLATE_DIR) \
-  --template $(MIA_TYPES_TYPE_TEMPLATE) \
-  --namespace types \
-  $2/$(name).json | $(TSFMT) --stdin > \
-  $(MIA_TYPES_LIB_DIR)/$(shell $(TRANSFORM) -t modulecase $(name)).ts && ) true
-endef
-
-$(MIA_TYPES_DIR): $(MIA_TYPES_LIB_DIR)
+$(MIA_TYPES_DIR): $(MIA_TYPES_DIR)/lib
 	touch $@
-
-$(MIA_TYPES_LIB_DIR): $(MIA_SCHEMA_DIR)\
-                      $(MIA_TYPES_SRC_DIR)\
-                      $(MIA_TYPES_TEMPLATE_DIR)
-	rm -R $@ 2> /dev/null || true 
-	mkdir $@
-	cp -R -u $(MIA_TYPES_SRC_DIR)/* $@
-
-	$(call mia_mktypes,\
-	$(MIA_TYPES_MODEL_NAMES),$(MIA_SCHEMA_MODELS_DIR))
-
-	$(call mia_mktypes,\
-	$(MIA_TYPES_TYPE_NAMES),$(MIA_SCHEMA_TYPES_DIR))
-
-	$(TSC) --project $@
-	touch $@
-
-$(MIA_TYPES_SRC_DIR): $(MIA_TYPES_SRC_DIR_FILES)
-	touch $@
-
-$(MIA_TYPES_TEMPLATE_DIR): $(MIA_TYPES_TEMPLATE_DIR_FILES)
-	touch $@
+	
+$(MIA_TYPES_DIR)/lib: $(MIA_SCHEMA_DIR)
+	rm -R $@ || true
+	mkdir -p $@
+	$(call qtl_data_types,$@,$(MIA_SCHEMA_FILES))
--- apps/mia/packages/mia-types/variables.mk
+++ apps/mia/packages/mia-types/variables.mk
@@ -1,17 +1,2 @@
 MIA_TYPES_DIR:=$(MIA_PACKAGES_DIR)/mia-types
-MIA_TYPES_LIB_DIR:=$(MIA_TYPES_DIR)/lib
-MIA_TYPES_SRC_DIR:=$(MIA_TYPES_DIR)/src
-MIA_TYPES_SRC_DIR_FILES:=$(shell find $(MIA_TYPES_SRC_DIR) -type f)
-MIA_TYPES_TYPE_TEMPLATE:=type.types
-
-# Directory with dagen templates.
-MIA_TYPES_TEMPLATE_DIR:=$(MIA_TYPES_DIR)/templates
-MIA_TYPES_TEMPLATE_DIR_FILES:=$(shell find $(MIA_TYPES_TEMPLATE_DIR) -type f)
-
-# This is the name of all the files in the model dir without extensions.
-MIA_TYPES_MODEL_NAMES=$(notdir $(basename $(wildcard \
-			$(MIA_SCHEMA_MODELS_DIR)/*.json)))
-MIA_TYPES_TYPE_NAMES=$(notdir $(basename $(wildcard \
-			$(MIA_SCHEMA_TYPES_DIR)/*.json))) 
-
-CLEAN_TARGETS:=$(CLEAN_TARGETS) $(MIA_TYPES_LIB_DIR)
+CLEAN_TARGETS:=$(CLEAN_TARGETS) $(MIA_TYPES_DIR)/lib
