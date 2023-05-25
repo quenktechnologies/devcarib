df5d6aadd1b44fad4ce6f10af96feeb1110b3293
Lasana Murray <lmurray@quenk.com>
2023-05-25 12:09:21 -0400

    #162: update mia-schema for latest libs [mia]

--- apps/mia/schema/variables.mk
+++ apps/mia/schema/variables.mk
@@ -1,3 +1,13 @@
 MIA_SCHEMA_DIR?=$(MIA_DIR)/schema
-MIA_SCHEMA_MODELS_DIR?=$(MIA_SCHEMA_DIR)/models
-MIA_SCHEMA_TYPES_DIR?=$(MIA_SCHEMA_DIR)/types
+MIA_SCHEMA_TARGETS?=$(foreach name,$(notdir $(basename $(wildcard \
+                      $(MIA_SCHEMA_DIR)/*.json))),$(MIA_SCHEMA_DIR)/$(name).json)
+
+MIA_SCHEMA_NAMES?=$(notdir $(basename $(MIA_SCHEMA_TARGETS)))
+MIA_SCHEMA_TYPE_NAMES?=login
+MIA_SCHEMA_MODEL_NAMES:=$(filter-out $(MIA_SCHEMA_TYPE_NAMES),$(MIA_SCHEMA_NAMES))
+
+MIA_SCHEMA_MODEL_TARGETS:=$(addsuffix .json,$(addprefix $(MIA_SCHEMA_DIR)/,\
+			  $(MIA_SCHEMA_MODEL_NAMES)))
+
+MIA_SCHEMA_FILES:=$(addsuffix .json,$(addprefix $(MIA_SCHEMA_DIR)/,\
+                  $(MIA_SCHEMA_NAMES)))
