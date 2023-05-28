b30e6f040ffd5b1da9b94b77b87363685d6eec93
Lasana Murray <lmurray@quenk.com>
2023-05-28 14:07:21 -0400

    #162: update remote models template [converse]

--- apps/converse/frontend/src/app/remote/models/build.mk
+++ apps/converse/frontend/src/app/remote/models/build.mk
@@ -1,25 +1,7 @@
+include ./node_modules/@quenk/dagen-templates-quenk/macros.mk
 
-$(CONVERSE_REMOTE_MODELS_DIR): $(CONVERSE_SCHEMA_DIR)\
-                               $(CONVERSE_REMOTE_MODELS_DIR)/templates
+$(CONVERSE_REMOTE_MODELS_DIR): $(CONVERSE_SCHEMA_DIR) $(CONVERSE_REMOTE_MODELS_FILES)
+	$(call qtl_remote_models,$@,$(CONVERSE_SCHEMA_MODEL_TARGETS))
 
-	$(foreach d,$(CONVERSE_REMOTE_MODELS_MODEL_NAMES), \
-	$(eval name=$(notdir $(basename $(d)))) \
-	$(DAGEN) --templates $@/templates \
-	--plugin $(DAGEN_PLUGIN_IMPORTS)\
-	--template $@/templates/model.nunjucks\
-	--check $@/checks.json \
-	--namespace rmodel \
-	$(CONVERSE_SCHEMA_MODELS_DIR)/$(name).json | $(TSFMT) --stdin > \
-	$@/$(shell $(TRANSFORM) -t modulecase $(name)).ts &&) true \
-
-	$(DAGEN) --templates $@/templates \
-	--template $@/templates/index.nunjucks \
-	--set models="$(CONVERSE_REMOTE_MODELS_MODEL_NAMES)" \
-	--namespace rmodel | \
-	$(TSFMT) --stdin > \
-	$@/index.ts
-
-	touch $@
-
-$(CONVERSE_REMOTE_MODELS_DIR)/templates: $(CONVERSE_REMOTE_MODELS_TEMPLATE_DIR_FILES)
+$(CONVERSE_REMOTE_MODELS_FILES):
 	touch $@
