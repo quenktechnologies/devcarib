7f9db2c5927eaab4185a22def8e4a19d2a796f13
Lasana Murray <lmurray@quenk.com>
2023-05-28 14:25:31 -0400

    #162: update build files for new templates [converse]

--- apps/converse/frontend/build.mk
+++ apps/converse/frontend/build.mk
@@ -1,20 +1,21 @@
-
-$(CONVERSE_FRONTEND_DIR): $(CONVERSE_FRONTEND_PUBLIC_DIR)
+$(CONVERSE_FRONTEND_DIR): $(CONVERSE_FRONTEND_PUBLIC_DIR) \
+                     $(CONVERSE_FRONTEND_FRONTEND_TEST_DIR)
 	touch $@
 
 $(CONVERSE_FRONTEND_PUBLIC_DIR): $(CONVERSE_FRONTEND_CSS_FILE)\
-                                 $(CONVERSE_FRONTEND_JS_FILE)
+                            $(CONVERSE_FRONTEND_JS_FILE)
 	touch $@
 
 $(CONVERSE_FRONTEND_JS_FILE): $(CONVERSE_FRONTEND_LIB_DIR)
 	mkdir -p $(dir $@)
-	$(BROWSERIFY) $(CONVERSE_FRONTEND_LIB_DIR)/main.js \
+	$(BROWSERIFY) -t envify $(CONVERSE_FRONTEND_LIB_DIR)/main.js | \
+	$(ENVIFY)\
 	$(if $(findstring yes,$(DEBUG)),,|$(UGLIFYJS)) > $@
 
-$(CONVERSE_FRONTEND_LIB_DIR): $(CONVERSE_FRONTEND_SRC_DIR)\
-	                      $(CONVERSE_REMOTE_MODELS_DIR)/*\
-	                      $(DEVCARIB_FRONTEND_DIR)\
-	                      $(DEVCARIB_WIDGETS_DIR)
+$(CONVERSE_FRONTEND_LIB_DIR): $(CONVERSE_FRONTEND_SRC_FILES) \
+			 $(CONVERSE_REMOTE_MODELS_DIR) \
+			 $(DEVCARIB_FRONTEND_DIR) \
+	                 $(DEVCARIB_WIDGETS_DIR)
 	rm -R $@ 2> /dev/null || true 
 	mkdir $@
 	cp -R -u $(CONVERSE_FRONTEND_SRC_DIR)/* $@
@@ -22,22 +23,34 @@ $(CONVERSE_FRONTEND_LIB_DIR): $(CONVERSE_FRONTEND_SRC_DIR)\
 	$(TSC) --project $@
 	touch $@
 
-$(CONVERSE_FRONTEND_SRC_DIR): $(CONVERSE_FRONTEND_SRC_FILES)\
-                              $(CONVERSE_REMOTE_MODELS_DIR)
-	touch $@
-
 include $(CONVERSE_REMOTE_MODELS_DIR)/build.mk
 
-$(CONVERSE_FRONTEND_CSS_FILE): $(DEVCARIB_WIDGETS_DIR)\
-			       $(CONVERSE_FRONTEND_LESS_IMPORTS)\
-                               $(CONVERSE_FRONTEND_LESS_MAIN)
+$(CONVERSE_FRONTEND_CSS_FILE): $(CONVERSE_FRONTEND_LESS_IMPORTS) \
+                          $(CONVERSE_FRONTEND_LESS_MAIN)
 	mkdir -p $(dir $@)
 	rm -R $@ || true
 	$(LESSC) --source-map-less-inline \
-	--js-vars=$(JS_VARS) $(CONVERSE_FRONTEND_LESS_MAIN) \
+	--js-vars=$(CONVERSE_FRONTEND_JS_VARS) $(CONVERSE_FRONTEND_LESS_MAIN) \
 	| ./node_modules/.bin/cleancss > $@
 
 $(CONVERSE_FRONTEND_LESS_IMPORTS): $(CONVERSE_FRONTEND_LESS_FILES)
 	echo "" > $@
 	$(foreach f,$(subst $(CONVERSE_FRONTEND_DIR),,$^),\
 	echo '@import "./$(f)";' >> $@ && ) true
+
+$(CONVERSE_FRONTEND_FRONTEND_TEST_DIR): $(CONVERSE_FRONTEND_FRONTEND_TEST_BUNDLE_DIR)
+	touch $@
+
+$(CONVERSE_FRONTEND_FRONTEND_TEST_BUNDLE_DIR): $(CONVERSE_FRONTEND_FRONTEND_TEST_BUILD_DIR)
+	rm -R $@ || true
+	mkdir -p $@
+	$(foreach test,$(shell find $(CONVERSE_FRONTEND_FRONTEND_TEST_BUILD_DIR)\
+	   -name \*.js), $(BROWSERIFY) $(test) > $@/$(notdir $(basename $(test))).js)
+
+$(CONVERSE_FRONTEND_FRONTEND_TEST_BUILD_DIR): $(CONVERSE_FRONTEND_FRONTEND_TEST_SRC_DIR)
+	rm -R $@ || true
+	cp -R $(CONVERSE_FRONTEND_FRONTEND_TEST_SRC_DIR) $@
+	$(TSC) --project $@
+
+$(CONVERSE_FRONTEND_FRONTEND_TEST_SRC_DIR): $(CONVERSE_FRONTEND_FRONTEND_TEST_SRC_DIR_FILES)
+	touch $@
