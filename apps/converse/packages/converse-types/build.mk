834c64c235d38ee39165b66462c31571ecf74efe
Lasana Murray <lmurray@quenk.com>
2023-05-28 15:52:00 -0400

    #162: update converse-types build files [converse-types]

--- apps/converse/packages/converse-types/build.mk
+++ apps/converse/packages/converse-types/build.mk
@@ -1,45 +1,9 @@
-### Builds converse-types. ###
-# Require:
-# 1. CONVERSE_TYPES_DIR 
-# 2. CONVERSE_SCHEMA_MODELS_DIR
-# 3. CONVERSE_SCHEMA_TYPES_DIR
+include ./node_modules/@quenk/dagen-templates-quenk/macros.mk
 
-### Macros ###
-
-# Make type files for schema in a dir.
-# $1 List of types to process
-# $2 The path to the dir the files are in.
-define types_mktypes
-$(foreach d,$1,\
-  $(eval name=$(notdir $(basename $(d)))) \
-  $(DAGEN) --templates $(CONVERSE_TYPES_TEMPLATE_DIR) \
-  --template $(CONVERSE_TYPES_TYPE_TEMPLATE) \
-  --namespace types \
-  $2/$(name).json | $(TSFMT) --stdin > \
-  $(CONVERSE_TYPES_LIB_DIR)/$(shell $(TRANSFORM) -t modulecase $(name)).ts && ) true
-endef
-
-$(CONVERSE_TYPES_DIR): $(CONVERSE_TYPES_LIB_DIR)
+$(CONVERSE_TYPES_DIR): $(CONVERSE_TYPES_DIR)/lib
 	touch $@
-
-$(CONVERSE_TYPES_LIB_DIR): $(CONVERSE_SCHEMA_DIR)\
-                           $(CONVERSE_TYPES_SRC_DIR)\
-                           $(CONVERSE_TYPES_TEMPLATE_DIR)
-	rm -R $@ 2> /dev/null || true 
-	mkdir $@
-	cp -R -u $(CONVERSE_TYPES_SRC_DIR)/* $@
-
-	$(call types_mktypes,\
-	$(CONVERSE_TYPES_MODEL_NAMES),$(CONVERSE_SCHEMA_MODELS_DIR))
-
-	$(call types_mktypes,\
-	$(CONVERSE_TYPES_TYPE_NAMES),$(CONVERSE_SCHEMA_TYPES_DIR))
-
-	$(TSC) --project $@
-	touch $@
-
-$(CONVERSE_TYPES_SRC_DIR): $(CONVERSE_TYPES_SRC_DIR_FILES)
-	touch $@
-
-$(CONVERSE_TYPES_TEMPLATE_DIR): $(CONVERSE_TYPES_TEMPLATE_DIR_FILES)
-	touch $@
+	
+$(CONVERSE_TYPES_DIR)/lib: $(CONVERSE_SCHEMA_DIR)
+	rm -R $@ || true
+	mkdir -p $@
+	$(call qtl_data_types,$@,$(CONVERSE_SCHEMA_FILES))
--- apps/converse/packages/converse-types/variables.mk
+++ apps/converse/packages/converse-types/variables.mk
@@ -1,15 +1,2 @@
 CONVERSE_TYPES_DIR:=$(CONVERSE_PACKAGES_DIR)/converse-types
-CONVERSE_TYPES_LIB_DIR:=$(CONVERSE_TYPES_DIR)/lib
-CONVERSE_TYPES_SRC_DIR:=$(CONVERSE_TYPES_DIR)/src
-CONVERSE_TYPES_SRC_DIR_FILES:=$(shell find $(CONVERSE_TYPES_SRC_DIR) -type f)
-CONVERSE_TYPES_TYPE_TEMPLATE:=type.types
-
-# Directory with dagen templates.
-CONVERSE_TYPES_TEMPLATE_DIR:=$(CONVERSE_TYPES_DIR)/templates
-CONVERSE_TYPES_TEMPLATE_DIR_FILES:=$(shell find $(CONVERSE_TYPES_TEMPLATE_DIR) -type f)
-
-# This is the name of all the files in the model dir without extensions.
-CONVERSE_TYPES_MODEL_NAMES=$(notdir $(basename $(wildcard \
-			$(CONVERSE_SCHEMA_MODELS_DIR)/*.json)))
-CONVERSE_TYPES_TYPE_NAMES=$(notdir $(basename $(wildcard \
-			$(CONVERSE_SCHEMA_TYPES_DIR)/*.json))) 
+CLEAN_TARGETS:=$(CLEAN_TARGETS) $(CONVERSE_TYPES_DIR)/lib
