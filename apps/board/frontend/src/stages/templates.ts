da12f4de5f9d316675c64b5836abdfdcd9502484
Lasana Murray <lmurray@quenk.com>
2023-07-16 18:05:11 -0400

    close #173: [board] add more error templates

--- apps/board/frontend/src/stages/templates.ts
+++ apps/board/frontend/src/stages/templates.ts
@@ -1,4 +1,7 @@
 export const templates = {
     notNull: 'This field is required',
-    invalid: 'The value entered is not valid.'
+    invalid: 'The value entered is not valid.',
+    matches: 'The value entered does not appear valid.',
+    maxLength: 'This field cannot have more than {target} characters.',
+    minLength: 'This field must be more than {target} characters.'
 };
